864001a5cfd6c6994ebd53db23e30684333dc702
shpaker <shpaker@gmail.com>
2017-04-16 18:53:55 +0300

    rem long description

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
-import os
+# import os
 
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+# def read(fname):
+#     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup (
     name='winregistry',
-    version='0.8',
+    version='0.8.2',
     author='Alexander Shpak',
     author_email='shpaker@gmail.com',
     description=('Library aimed at working with Windows registry'),
-    long_description=read('README.rst'),
+    # long_description=read('readme.rst'),
     keywords='windows registry regedit winreg',
     url='https://github.com/shpaker/winregistry',
     platforms='windows',
